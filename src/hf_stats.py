ddf203185eb44dbeaafa02c4360722ea27d3e34b
noamteyssier <nb.teyssier@gmail.com>
2018-08-21 16:55:34 -0700

    edit : moved to python3, updated input arguments to hf_stats, replaced print_out methods to print to baseName and tab delim stats file

--- src/hf_stats.py
+++ src/hf_stats.py
@@ -7,91 +7,6 @@ import subprocess
 
 from modules import Diversity_Window
 
-# NEED TO FIX:
-# PRINT AS REVERSE COMPLEMENT IF TARGET SEQUENCE IS REVERSE
-#
-# class seqio:
-#     def __init__(self, id, seq):
-#         self.id = id[1:-1]
-#         self.seq = seq[:-1]
-#         self.pair_bool = False
-#         self.target = None
-#     def add_target(self, target):
-#         self.target = target
-#     def switch_bool(self):
-#         self.pair_bool = True
-#     def write_fasta(self, window, fn):
-#         fn.write('>' + self.id + '_' + window.id + '\n')
-#         if '_forward' in self.target.id:
-#             fn.write(self.seq + '\n')
-#         else:
-#             quick_seq = Seq(self.seq)
-#             fn.write(str(quick_seq.reverse_complement()) + '\n')
-# class Diversity_Window:
-#     windows = list()
-#     dictionary = dict()
-#     def __init__(self, id):
-#         self.id = id
-#         self.targets = list()
-#         self.hf_count = 0
-#         self.left = None
-#         self.right = None
-#         Diversity_Window.windows.append(self)
-#     def add_target(self, target):
-#         """add to list of targets"""
-#         self.targets.append(target)
-#         if 'start_forward' in target.id:
-#             self.left = target
-#         elif 'end_reverse' in target.id:
-#             self.right = target
-#     def increment_hf(self):
-#         self.hf_count += 1
-#     def left_cuts(self):
-#         return self.left.total_reads
-#     def right_cuts(self):
-#         return self.right.total_reads
-#     def total_cuts(self):
-#         return self.left_cuts() + self.right_cuts()
-#     def print_stats(self):
-#         stats = [self.total_cuts(), self.left_cuts(), self.right_cuts(), self.hf_count, self.id]
-#         return [str(s) for s in stats]
-# class Target:
-#     targets = list()
-#     lookup = dict()
-#     lookup_full_seq = dict()
-#     def __init__(self, seqio):
-#         """add self to target list and into dictionary for lookup"""
-#         self.seqio = seqio
-#         self.id = seqio.id
-#         self.seq = str(seqio.seq)
-#         self.window = None
-#
-#         # master list of reads
-#         self.pairs = list()
-#
-#         # total counts
-#         self.total_reads = 0
-#         self.z_count = 0
-#         self.o_count = 0
-#         self.high_fidelity = 0
-#
-#         Target.targets.append(self)
-#         Target.lookup[self.seq] = self
-#     def assign_window(self, window):
-#         """assign window to target for easy link"""
-#         self.window = window
-#     def add_read(self, seq, pos=0):
-#         """add read to list of reads"""
-#         self.total_reads += 1
-#         if pos == 0:
-#             self.z_count += 1
-#         if pos == 1:
-#             self.o_count += 1
-#         Target.lookup_full_seq[seq] = self
-#     def add_pair(self, pair):
-#         self.pairs.append(pair)
-#         self.high_fidelity += 1
-
 def parse_targets(target_filename):
     """
     - create Target & Diversity_Window classObjects
@@ -195,29 +110,51 @@ def assign_grepped(input, targets, genDir):
             if len(pair_uid[uid]) == 2:
                 window.increment_highFidelityCount()
                 yield [window, uid] + [seqID_to_grepSeq[seqID] for seqID in pair_uid[uid]]
-def print_stats(output, total):
-    with open(output+'.csv', 'w+') as f:
-        f.write('total_reads, total_cuts, left_cuts, right_cuts, high_fidelity_pairs, name\n')
-        for w in Diversity_Window.windows:
-            f.write(str(total) + ',' + ','.join(w.print_stats()) + '\n')
-def hf_fasta(output, pairs):
-    with open(output+'.fasta', 'w+') as f:
-        for window in pairs:
-            for uid in pairs[window]:
-                if len(pairs[window][uid]) == 2: # only print high fidelity reads
-                    for s in pairs[window][uid]:
-                        direction, seq = s.split('_') # separate direction from seq (added in assigned_grepped)
-                        header = [uid, direction, window]
-                        if '_reverse' in Target.lookup_full_seq[seq].id: # write reverse complement
-                            quick_seq = Seq(seq)
-                            seq = str(quick_seq.reverse_complement())
-                        f.write('_'.join(header) + '\n' + seq + '\n')
+def write_stats(statsOut, totalReads):
+    """
+    Write cut statistics for each window to a tab delim file
+    """
+    header = [
+        'totalReads',
+        'totalCuts',
+        'leftCuts',
+        'rightCuts',
+        'highFidelityCount',
+        'window'
+        ]
 
+    statsOut.write('\t'.join(header)+'\n')
+    for window in Diversity_Window.windows:
+        writeOut = [
+            totalReads,
+            window.get_totalCuts(),
+            window.get_leftCuts(),
+            window.get_rightCuts(),
+            window.get_highFidelityCount(),
+            window.name
+            ]
+        statsOut.write('\t'.join(writeOut)+'\n')
+def write_fasta(fastaOut, window, uid, forwardSeq, reverseSeq):
+    """
+    write a fasta entry for each sequence giving appropriate direction
+    and window information
+    """
+    fasta_entry = [
+    '_'.join(
+        [uid, 'forward', window.name]
+        ),
+    forwardSeq,
+    '_'.join(
+        [uid, 'reverse', window.name]
+        ),
+    reverseSeq
+    ]
 
+    fastaOut.write('\n'.join(fasta_entry)+'\n')
 def main():
     parser = argparse.ArgumentParser(description='create high fidelity statistics')
     parser.add_argument('-t', '--targets', help='filename of target sequences', required=True)
-    parser.add_argument('-b', '--basename', help='basename to preappend to tab.txt and fasta output', required=True)
+    parser.add_argument('-b', '--baseName', help='baseName to preappend to tab.txt and fasta output', required=True)
     parser.add_argument('-i', '--input', help='fasta to grep reads and assign to targets')
     parser.add_argument('-d', '--directory', help='generated_files directory to write to')
     args = parser.parse_args()
@@ -226,27 +163,20 @@ def main():
     input = args.input
     targets = args.targets
     genDir = args.directory
-    basename = args.basename
+    baseName = args.baseName
+    outName = genDir+baseName+'_hf'
 
-    ## these arguments would get around having to ask for another argument to calculate total reads
-    # run grep reads as subprocess
-    # args.input = fasta to grep target sequences in
-    # args.targets = tab.txt to create regex and grep with
-    # args.basename = basename to preappend to output files
-
+    statsOut = open(outName+'.tab.txt', 'w+')
+    fastaOut = open(outName+'.fa', 'w+')
 
     # # methods
     parse_targets(targets)
-    calculate_total(input)
-    for assignedPairList in assign_grepped(input, targets, genDir):
-        window, uid, forwardSeq, reverseSeq = assignedPairList
+    totalReads = calculate_total(input)
+    for window, uid, forwardSeq, reverseSeq in assign_grepped(input, targets, genDir):
+        write_fasta(fastaOut, window, uid, forwardSeq, reverseSeq)
 
+    write_stats(statsOut, totalReads)
 
-
-    # # printing methods
-    # print_stats(output, total)
-    # hf_fasta(output, pairs)
-    #
 
 
 if __name__ == '__main__':
